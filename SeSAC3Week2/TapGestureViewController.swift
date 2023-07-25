2c90abec6092608c434bc34f8b86c37d9d708be4
SSABOODA <ssaboo92@gmail.com>
2023-07-25 09:32:19 +0900

    TapGesture Test

---
+++ SeSAC3Week2/TapGestureViewController.swift
@@ -0,0 +1,20 @@
+//
+//  TapGestureViewController.swift
+//  SeSAC3Week2
+//
+//  Created by 한성봉 on 2023/07/25.
+//
+
+import UIKit
+
+class TapGestureViewController: UIViewController {
+    
+    // 이미지 TapGesture 테스트 하기
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    
+    
+    
+}
---
+++ SeSAC3Week2/TestViewController.swift
@@ -0,0 +1,19 @@
+//
+//  TestViewController.swift
+//  SeSAC3Week2
+//
+//  Created by 한성봉 on 2023/07/24.
+//
+
+import UIKit
+
+class TestViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func unwindToHome(_ seque: UIStoryboardSegue) {
+    }
+
+}
