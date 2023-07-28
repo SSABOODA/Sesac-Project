49b7d531aa5c695a8c3a677038429c46ab352019
SSABOODA <ssaboo92@gmail.com>
2023-07-28 09:34:03 +0900

    코드 정리

--- Assignment/ShoppingTableViewController.swift
+++ Assignment/ShoppingTableViewController.swift
@@ -18,11 +18,8 @@ class ShoppingTableViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.rowHeight = 50
-        
         designView()
         designButton()
-        
-        
     }
     
     @IBAction func addButtonClicked(_ sender: UIButton) {
@@ -34,22 +31,10 @@ class ShoppingTableViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return shoppingList.count
     }
-    
-    // checkmark.square
-    // checkmark.square.fill
-    // star
-    // star.fill
-    
+
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "shoppingCell")!
-        
-        
-        let imgView = UIImageView(frame: CGRect(x: 0, y: 0, width: 30, height: 30))
-        imgView.image = UIImage(systemName: "start")
-        
         cell.textLabel?.text = shoppingList[indexPath.row]
-        cell.imageView?.image = UIImage(systemName: "heart")
-        cell.accessoryView = imgView
         return cell
     }
     
@@ -63,10 +48,4 @@ class ShoppingTableViewController: UITableViewController {
         addButton.clipsToBounds = true
         addButton.backgroundColor = .systemGray5
     }
-
-    
-    
-    
-    
-    
 }
