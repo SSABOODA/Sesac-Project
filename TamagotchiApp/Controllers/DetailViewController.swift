c1c5b93af481c41f4242429891c765abde83d07b
SSABOODA <ssaboo92@gmail.com>
2023-08-05 01:12:05 +0900

    detailView Level 변화 기능 구현

--- TamagotchiApp/Controllers/DetailViewController.swift
+++ TamagotchiApp/Controllers/DetailViewController.swift
@@ -27,6 +27,8 @@ class DetailViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        saveUserDefalutData()
 
         configureDetailView()
         designDetailView()
@@ -34,55 +36,114 @@ class DetailViewController: UIViewController {
         designRightBarButtonItem()
         
         title = "대장님의 다마고취"
+        
+        
+    }
+    
+    func saveUserDefalutData() {
+        if tamagotchi != nil {
+            
+            tamagotchi!.rice = UserDefaults.standard.integer(forKey: "riceCount")
+            tamagotchi!.water = UserDefaults.standard.integer(forKey: "waterCount")
+            tamagotchi!.imageName = UserDefaults.standard.string(forKey: "imageName") ?? tamagotchi!.imageName
+            tamagotchi!.level = UserDefaults.standard.integer(forKey: "level")
+            
+        }
+        
     }
     
     override func viewDidLayoutSubviews() {
         designTextField(riceTextField)
         designTextField(waterTextField)
     }
+ 
+    @IBAction func tapGestureTapped(_ sender: UITapGestureRecognizer) {
+        view.endEditing(true)
+    }
     
+    @IBAction func eatRiceTextFieldClicked(_ sender: UITextField) {
+    }
+    
+    @IBAction func eatWaterTextFieldClicked(_ sender: UITextField) {
+    }
     
     @IBAction func eatRiceButtonClicked(_ sender: UIButton) {
         if tamagotchi != nil {
-            tamagotchi!.rice += 1
+            guard let text = riceTextField.text else { return }
+            if !text.isEmpty && (Int(text) != nil) {
+                tamagotchi!.rice += Int(text)!
+            } else {
+                tamagotchi!.rice += 1
+            }
+            riceTextField.text = ""
+            checkTamagotchiLevel(tamagotchi!)
+            tamagotchiLevelLabel.text = tamagotchi!.currentLevelText
+            UserDefaults.standard.set(tamagotchi!.rice, forKey: "riceCount")
         }
-        
-        tamagotchiLevelLabel.text = tamagotchi?.currentLevelText
-        checkTamagotchiLevel()
     }
-    
-    
     
     @IBAction func eatWaterButtonClicked(_ sender: UIButton) {
         if tamagotchi != nil {
-            tamagotchi!.water += 1
+            guard let text = waterTextField.text else { return }
+            if !text.isEmpty && (Int(text) != nil) {
+                tamagotchi!.water += Int(text)!
+            } else {
+                tamagotchi!.water += 1
+            }
+            waterTextField.text = ""
+            checkTamagotchiLevel(tamagotchi!)
+            tamagotchiLevelLabel.text = tamagotchi!.currentLevelText
+            UserDefaults.standard.set(tamagotchi!.rice, forKey: "waterCount")
+        }
+    }
+    
+    func checkTamagotchiLevel(_ data: Tamagotchi) {
+        
+//        print("밥알: \(data.rice)")
+//        print("물방울: \(data.water)")
+        
+        let riceCount = data.rice
+        let waterCount = data.water
+        let result = (Double(riceCount) / 5.0) + (Double(waterCount) / 2.0)
+        print("result: \(result)")
+        switch Int(result) {
+        case 0..<10:
+            tamagotchi?.level = 1
+        case 10..<20:
+            tamagotchi?.level = 1
+        case 20..<30:
+            tamagotchi?.level = 2
+        case 30..<40:
+            tamagotchi?.level = 3
+        case 40..<50:
+            tamagotchi?.level = 4
+        case 50..<60:
+            tamagotchi?.level = 5
+        case 60..<70:
+            tamagotchi?.level = 6
+        case 70..<80:
+            tamagotchi?.level = 7
+        case 80..<90:
+            tamagotchi?.level = 8
+        case 90..<100:
+            tamagotchi?.level = 9
+        case 100...:
+            tamagotchi?.level = 10
+        default:
+            tamagotchi?.level = 10
         }
         
-        tamagotchiLevelLabel.text = tamagotchi?.currentLevelText
-        checkTamagotchiLevel()
+        if tamagotchi != nil {
+            UserDefaults.standard.set(tamagotchi!.level, forKey: "level")
+            
+            if let num = tamagotchi!.imageName.first {
+                let imageNameString = "\(String(num))-\(tamagotchi!.level)"
+                tamagotchiImageView.image = UIImage(named: imageNameString)
+                UserDefaults.standard.set(imageNameString, forKey: "imageName")
+            }
+        }
     }
     
-    func checkTamagotchiLevel() {
-        guard let tamagotchi else { return }
-        print("밥알: \(tamagotchi.rice)")
-        print("물방울: \(tamagotchi.water)")
-        
-        let riceCount = tamagotchi.rice
-        let waterCount = tamagotchi.water
-        let result = (riceCount / 5) + (waterCount / 2)
-        
-        let currentImageNum = Int(tamagotchi.imageName.split(separator: "-").last!)!
-        
-//        switch result {
-//        case 0...10:
-//            tamagotchi.
-//        default:
-//            <#code#>
-//        }
-    }
-    
-    
-   
     func configureDetailView() {
         guard let tamagotchi else { return }
         speechBubbleImageView.image = UIImage(named: "bubble")
@@ -122,6 +183,8 @@ class DetailViewController: UIViewController {
         textField.textAlignment = .center
         textField.textColor = UIColor.systemGray2
         textField.layer.masksToBounds = true
+        
+        textField.delegate = self
     }
     
     func designButton(_ button: UIButton) {
@@ -136,5 +199,15 @@ class DetailViewController: UIViewController {
     
     func designRightBarButtonItem() {
         self.navigationItem.rightBarButtonItem?.tintColor = .lightGray
+    }
+}
+
+
+extension DetailViewController: UITextFieldDelegate {
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+//        print(#function)
+    }
+    func textFieldDidEndEditing(_ textField: UITextField) {
+//        print(#function)
     }
 }
