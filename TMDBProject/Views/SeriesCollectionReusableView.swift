8379670c97d07090310137d8437e96198b7b180e
SSABOODA <ssaboo92@gmail.com>
2023-08-16 17:27:31 +0900

    collectionview cell, collection reusable view 세팅

---
+++ TMDBProject/Views/SeriesCollectionReusableView.swift
@@ -0,0 +1,23 @@
+//
+//  SeriesCollectionReusableView.swift
+//  TMDBProject
+//
+//  Created by 한성봉 on 2023/08/16.
+//
+
+import UIKit
+
+class SeriesCollectionReusableView: UICollectionReusableView {
+
+    
+    @IBOutlet var seasonLabel: UILabel!
+    @IBOutlet var episodeCountLabel: UILabel!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+
+        
+    }
+    
+}
---
+++ TMDBProject/Views/SeriesCollectionViewCell.swift
@@ -0,0 +1,35 @@
+//
+//  SeriesCollectionViewCell.swift
+//  TMDBProject
+//
+//  Created by 한성봉 on 2023/08/16.
+//
+
+import UIKit
+
+class SeriesCollectionViewCell: UICollectionViewCell {
+
+    
+    
+    @IBOutlet var posterImageView: UIImageView!
+    @IBOutlet var episodeLabel: UILabel!
+    
+    @IBOutlet var titleLabel: UILabel!
+    @IBOutlet var runtimeLabel: UILabel!
+    
+    @IBOutlet var overviewLabel: UILabel!
+    
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    
+    
+    
+    
+    
+    
+    
+}
